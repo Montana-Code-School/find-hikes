9c32b93a6304e97ac70b36248fd43f8e861f5cf8
Ian Oberbillig <Ian@Ians-MacBook-Pro-2.local>
2018-02-22 16:45:07 -0700

    MVP... not

--- hikr/src/js/Components/button.js
+++ hikr/src/js/Components/button.js
@@ -1,17 +1,32 @@
 import React, {Component} from 'react';
 import SearchFull from "../../css/imgs/hikrProtoSearchFull.png";
 import '../../css/button.css';
-import Droopdown1 from "./dropdown1.js";
-import Droopdown2 from "./dropdown2.js";
 
 class GoButton extends Component{
-  render(){
-    const imgClick = ()=> {
-      console.log(Droopdown1);
+  constructor(){
+    super();
+    this.state = {
+      buttonReturn : ""
     };
+  }
+  update = (e) => {
+    this.props.onClick(e.value);
+    this.setState({
+      buttonReturn: e.value
+    });
+  }
+  imgClick = ()=> {
+   console.log(this.props.passedDiff);
+   console.log(this.props.passedMiles);
+
+ }
+  render(){
+
     return(
       <div>
-        <img id="hikebutton" src={SearchFull} onClick = {() => imgClick()}/>
+        <div>
+          <img id="hikebutton" src={SearchFull} onClick = {this.update, this.imgClick}value= {this.state.buttonReturn}/>
+        </div>
       </div>
     );
   }
--- hikr/src/js/Components/dropdown1.js
+++ hikr/src/js/Components/dropdown1.js
@@ -6,12 +6,24 @@ const options = [
   'Medium',
   'Long'
 ];
-const defaultOption = options[1];
 class Droopdown1 extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      ddval1: ""
+    };
+  }
+  update = (e) => {
+    console.log(e.value);
+    this.props.onChange(e.value);
+    this.setState({
+      ddval1: e.value,
+    });
+  }
   render() {
     return(
       <div>
-        <Dropdown options={options} onChange={this._onSelect} value={defaultOption}/>
+        <Dropdown options={options} onChange={this.update} value={this.state.ddval1}/>
       </div>
     );
   }
